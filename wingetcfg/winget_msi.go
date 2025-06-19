50a55b4c2bc53a32afd0f7a9a39d99d858db3ef2
Miguel Cabrerizo <doncicuto@gmail.com>
2025-06-19 14:52:58 +0200

    feat: constants for MSI Package resource

---
+++ wingetcfg/winget_msi.go
@@ -0,0 +1,11 @@
+package wingetcfg
+
+const (
+	FileHashMD5              string = "MD5"
+	FileHasRIPEMD160         string = "RIPEMD160"
+	FileHashSHA1             string = "SHA1"
+	FileHashSHA256           string = "SHA256"
+	FileHashSHA384           string = "SHA384"
+	FileHashSHA512           string = "SHA512"
+	WinGetMSIPackageResource        = "xPSDesiredStateConfiguration/xMsiPackage"
+)
