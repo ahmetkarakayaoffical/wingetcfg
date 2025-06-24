180c0af570dbc2158bfd16eb60985467a1b23152
Miguel Cabrerizo <doncicuto@gmail.com>
2025-06-24 11:12:53 +0200

    feat: add powershell resource

---
+++ wingetcfg/winget_powershell.go
@@ -0,0 +1,17 @@
+package wingetcfg
+
+const (
+	OpenUEMPowershell = "openuem/Powershell"
+)
+
+// As there's no specific Powershell DSC that runs powershell scripts we create a custom resource
+func ExecutePowershellScript(pwshell string) (*WinGetResource, error) {
+	r := WinGetResource{}
+	r.Resource = OpenUEMPowershell
+
+	// Settings
+	r.Settings = map[string]any{}
+	r.Settings["Script"] = pwshell
+
+	return &r, nil
+}
