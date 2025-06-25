5d9433af7cf32343ab82520c231de7932233b877
Miguel Cabrerizo <doncicuto@gmail.com>
2025-06-25 13:04:54 +0200

    feat: add task ID to powershell script execute

--- wingetcfg/winget_powershell.go
+++ wingetcfg/winget_powershell.go
@@ -5,12 +5,13 @@ const (
 )
 
 // As there's no specific Powershell DSC that runs powershell scripts we create a custom resource
-func ExecutePowershellScript(name string, pwshell string, run string) (*WinGetResource, error) {
+func ExecutePowershellScript(id string, name string, pwshell string, run string) (*WinGetResource, error) {
 	r := WinGetResource{}
 	r.Resource = OpenUEMPowershell
 
 	// Settings
 	r.Settings = map[string]any{}
+	r.Settings["ID"] = id
 	r.Settings["Script"] = pwshell
 	r.Settings["ScriptRun"] = run
 	r.Settings["Name"] = name
